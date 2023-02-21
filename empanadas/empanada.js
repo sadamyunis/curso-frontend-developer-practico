98f14b4977612502719658a64cfd677a71f066f0
¨sadam <sadanyunis@hotmail.com>
2023-02-20 20:53:08 -0500

    estesiesmiprimerproyecto

---
+++ empanadas/empanada.js
@@ -0,0 +1,38 @@
+const menuempanadas = document.querySelector(`.boton1`);
+const desktop = document.querySelector(`.desktop`);
+
+
+menuempanadas.addEventListener(`click`,toggleingredientes1);
+
+function toggleingredientes1() {desktop.classList.toggle(`inactive`)};
+
+
+
+
+const menuempanadas2 = document.querySelector(`.boton2`);
+const desktop2 = document.querySelector(`.deskto`);
+
+
+menuempanadas2.addEventListener(`click`,toggleingredientes2);
+
+function toggleingredientes2() {desktop2.classList.toggle(`inactive`)};
+
+
+const menuempanadas3 = document.querySelector(`.boton3`);
+const desktop3 = document.querySelector(`.deskt`);
+
+
+menuempanadas3.addEventListener(`click`,toggleingredientes3);
+
+function toggleingredientes3() {desktop3.classList.toggle(`inactive`)};
+
+
+
+const menuempanadas4 = document.querySelector(`.boton4`);
+const desktop4 = document.querySelector(`.desk`);
+
+
+menuempanadas4.addEventListener(`click`,toggleingredientes4);
+
+function toggleingredientes4() {desktop4.classList.toggle(`inactive`)};
+
